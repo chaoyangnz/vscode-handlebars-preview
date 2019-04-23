77cb91c58cb60d6212f9a38452e2db3a7024fdec
raf <rafael.nicdao@cooltrax.com>
2019-04-23 21:55:39 +1000

    Almost a complete rewrite which includes the following changes: - Use webview API due to previewHtml deprecation - Support specifying path for a context file but also supports backwards-compatibility (i.e. look for *.hbs.js and *.hbs.json)

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,143 @@
-import {
-    workspace, window, commands,
-    ExtensionContext, TextEditorSelectionChangeEvent, TextDocumentChangeEvent
-} from "vscode";
-
-import PreviewContentProvider from './lib/PreviewContentProvider';
-import PREVIEW_URL from './lib/PREVIEW_URI';
-import preview from './lib/preview';
+import { commands, ExtensionContext, ViewColumn, window, workspace, TextDocument } from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as Handlebars from 'handlebars';
+import *  as os from 'os';
 
 export function activate(context: ExtensionContext) {
-
-    let provider = new PreviewContentProvider();
-
-    context.subscriptions.push(
-        // Preview providers
-        workspace.registerTextDocumentContentProvider("handlebars-preview", provider),
-
-        // Global handlers
-        window.onDidChangeTextEditorSelection((e: TextEditorSelectionChangeEvent) => {
-            if (e.textEditor === window.activeTextEditor) {
-                provider.update(PREVIEW_URL);
-            }
-        }),
-        workspace.onDidChangeTextDocument((e: TextDocumentChangeEvent) => {
-            if (e.document === window.activeTextEditor.document) {
-                provider.update(PREVIEW_URL);
-            }
-        }),
-
-        // Commands
-        commands.registerCommand('handlebarsPreview.preview', preview)
-    );
+  context.subscriptions.push(
+    // Commands
+    commands.registerCommand('handlebarsPreview.preview', () => {
+      const panel = window.createWebviewPanel("preview", "Handlebars HTML Preview", ViewColumn.Two, {})
+      panel.webview.html = getWebviewContent();
+    })
+  );
 }
 
-export function deactivate() {
+export interface HelperFunction {
+  name: string;
+  body: Handlebars.HelperDelegate;
+}
+export interface Context {
+  data: object;
+  helperFns: HelperFunction[];
+}
+
+function getWebviewContent(): string {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Handlebars HTML Preview</title>
+</head>
+<body>
+  ${getWebviewBody()}
+</body>
+</html>`;
+}
+
+function getWebviewBody(): string {
+  const { activeTextEditor } = window;
+  if (!activeTextEditor || !activeTextEditor.document) {
+    return 'Select handlebars file to render';
+  }
+
+  const { document: currentActiveFile } = activeTextEditor;
+  const ctxResolution = specifiedContext(activeTextEditor.document);
+
+  if (ctxResolution.error !== undefined) {
+    return ctxResolution.error;
+  }
+
+  let context: Context;
+  if (ctxResolution.context) {
+    context = ctxResolution.context;
+  } else {
+    // Go with the old format which is: look for *.hbs.json and *.hbs.js
+    const dataFilePath = `${currentActiveFile.fileName}.json`;
+    const helperFnsFilePath = `${currentActiveFile.fileName}.js`;
+
+    const data = fs.existsSync(dataFilePath)
+      ? JSON.parse(fs.readFileSync(dataFilePath, 'utf8'))
+      : {};
+
+    const helperFns = fs.existsSync(helperFnsFilePath)
+      ? requireUncached(helperFnsFilePath)
+      : [];
+
+    try {
+      context = { data, helperFns }
+    } catch (e) {
+      return `Error parsing data and/or helper functions source file: ${e}`;
+    }
+  }
+
+  try {
+    const templateContent = currentActiveFile.getText();
+    const renderedTemplate = renderTemplate(templateContent, context);
+
+    return `
+    result:
+    <br/>
+    ${renderedTemplate}
+    <br/>
+    <br/>
+    data:
+    <pre>${JSON.stringify(context.data, null, 2)}</pre>
+    <br/>
+    <br/>
+    helper functions: 
+    <pre>${JSON.stringify(context.helperFns.map(fn => ({ ...fn, body: fn.body.toString() })), null, 2)}</pre>
+    `
+  } catch (e) {
+    return `Error to do with handlebars compilation: ${e}`;
+  }
+}
+
+export function renderTemplate(template: string, context: Context) {
+  context.helperFns.forEach(fn => Handlebars.registerHelper(fn.name, fn.body))
+  const compiledTemplate = Handlebars.compile(template);
+  return compiledTemplate(context.data);
+}
+
+type ContextResolutionResult =
+  | { error: undefined, context: Context | undefined }
+  | { error: string };
+
+/** Looks at the first line of the handlebars template for `{{!-- vscode-handlebars-preview-context-source=<path.json> --}}` */
+function specifiedContext(doc: TextDocument): ContextResolutionResult {
+  if (doc.lineCount < 1) {
+    return { error: `Current active document has ${doc.lineCount} lines` };
+  }
+
+  const docFirstLine = doc.lineAt(0).text;
+  const matches = /\{\{!--\s*vscode-handlebars-preview-context-source=(.*)\s*--\}\}/.exec(docFirstLine);
+  if (!matches || matches.length < 2) {
+    return { error: undefined, context: undefined };
+  }
+
+  const activeFileDirName = path.dirname(doc.fileName);
+  const ctxFilePath = matches[1].trim();
+  const ctxAbsFilePath = path.join(activeFileDirName, ctxFilePath);
+  const ctxFileExists = fs.existsSync(ctxAbsFilePath);
+  if (!ctxFileExists) {
+    return { error: `Referenced context file ${ctxAbsFilePath} not found` };
+  }
+
+  try {
+    // TODO: Maybe use io-ts for validating correct structure
+    const ctx = requireUncached(ctxAbsFilePath) as Context;
+    if (!ctx.data) return { error: `Expected field data in context: ${JSON.stringify(ctx, null, 2)}` };
+    if (!ctx.helperFns) return { error: `Expected field helperFns in context: ${JSON.stringify(ctx, null, 2)}` };
+
+    return { error: undefined, context: ctx };
+  } catch (e) {
+    return { error: `Error occurred in importing module ${ctxAbsFilePath}: ${e}` };
+  }
+}
+
+function requireUncached(modulePath: string) {
+  delete require.cache[require.resolve(modulePath)];
+  return require(modulePath);
 }
